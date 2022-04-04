73cd29dcc3e2334dc9c45d46439681ae4e0ebf9b
SongMinGyu <ggp04114@naver.com>
2022-04-04 17:50:34 +0900

    test update2

--- app/src/main/java/com/example/lostandfind/data/Post.java
+++ app/src/main/java/com/example/lostandfind/data/Post.java
@@ -1,7 +1,7 @@
 package com.example.lostandfind.data;
 
 public class Post {
-    public String firstName, lastName; // Name SongMinGyu update!!!!
+    public String firstName, lastName; // Name SongMinGyu update!!!! update2222
     public long age;
     public Post(){}
     public Post(String firstName, String lastName, long age) {
